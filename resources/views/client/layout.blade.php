4e92af435b5a1111ef3bdf40cbb5ef5be550b517
bcombusiness <bcombusiness7800@gmail.com>
2025-02-25 21:41:14 +0700

    Tao file layout cho Client

---
+++ resources/views/client/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ asset("asset/client/css/bootstrap.min.css") }}">
+    <link rel="stylesheet" href="{{ asset("asset/client/css/main.css") }}">
+
+    <title>Shop B</title>
+</head>
+<body>
+    
+
+
+
+
+</body>
+
+    <script src="{{ asset("asset/client/js/bootstrap.bundle.min.js") }}"></script>
+
+</html>
